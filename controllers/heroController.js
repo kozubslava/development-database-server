f17f73c8c341f453db5a14b2424a7403602ecaac
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2024-03-19 00:16:23 +0200

    add update and create hero

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -1,39 +1,46 @@
+const { Hero } = require("../models");
+
 module.exports.createHero = async (req, res, next) => {
   try {
-
+    const { body } = req;
+    const hero = await Hero.create(body);
+    res.status(201).send({ data: hero });
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.getHero = async (req, res, next) => {
   try {
-
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.getHeros = async (req, res, next) => {
   try {
-
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.updateHero = async (req, res, next) => {
   try {
+    const { body, params: {heroId}} = req;
+    const hero = await Hero.findByPk(heroId);
+    const updatedHero = await hero.update(body, {
+      returning: true,
+    });
 
+    res.status(200).send({ data: updatedHero });
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.deleteHero = async (req, res, next) => {
   try {
-
   } catch (error) {
     next(error);
   }
-}
+};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const exress = require('express');
+const express = require('express');
 const router = require('./routers')
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 const PORT = 5000;
 
